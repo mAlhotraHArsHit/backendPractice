5d6ac5e46d7ab2db89783601e80fd6023cc4f30e
Harshit Malhtora <harshitmalhtora110@gmail.com>
2024-11-20 14:28:26 +0530

    MongoDB connected successfully

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from 'express';
+// const app=express()
+
+// (async()=>{
+//     try{
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//        app.on("error",(error)=>{
+//         console.log("Error present: ",error);
+//         throw error
+//        })
+//        app.listen(process.env.PORT,()=>{
+//         console.log(`App is listening on ${process.env.PORT}`);
+        
+//        })
+//     } catch(error){
+//         console.error("Error: ",error);
+//         throw err
+//     }
+// })
