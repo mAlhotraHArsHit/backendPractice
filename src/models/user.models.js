98df0e6e5c66cad570936bd7a8fdfb70efa25f66
Harshit Malhtora <harshitmalhtora110@gmail.com>
2024-11-21 16:54:06 +0530

    v3

---
+++ src/models/user.models.js
@@ -0,0 +1,56 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken"
+import brcypt from "bcrypt"
+
+
+const userSchema = new Schema({
+    username:{
+        type:String,
+        required: true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required: true,
+        unique:true,
+        lowercase:true,
+        trim:true
+    },
+    fullname:{
+        type:String,
+        required: true,
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+    avatar:{
+        type:String ,// cloudinary url
+        required:true
+    },
+    coverImage:{
+        type:String
+    },
+    watchHistory:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Video"
+        }
+    ],
+    password:{
+        type:String,
+        required: [true,'Password is required']
+    },
+    refreshToken:{
+        type:String
+    }
+},{timestamps:true});
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+export const User=mongoose.model("User",userSchema)
